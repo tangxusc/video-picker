31a60719f01ff43735e36dc9c3671ad8e54657df
苏连云 <562050688@qq.com>
2019-12-05 23:53:49 +0800

    修正Start未go启动

--- pkg/dispatcher/dispatcher.go
+++ pkg/dispatcher/dispatcher.go
@@ -18,7 +18,7 @@ func NewDispatcher(maxWorkers int) *dispatcher {
 	d := &dispatcher{
 		MaxWorkers: maxWorkers,
 		workers:    make(chan chan Job, maxWorkers),
-		jobs:       make(chan Job, maxWorkers),
+		jobs:       make(chan Job),
 	}
 	if maxWorkers < 1 {
 		panic("worker必须至少1个以上")
@@ -37,17 +37,19 @@ func (d *dispatcher) Start(ctx context.Context) {
 	for i := 0; i < d.MaxWorkers; i++ {
 		newWorker().Start(ctx, d.workers)
 	}
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case target, ok := <-d.jobs:
-			if !ok {
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
 				return
+			case target, ok := <-d.jobs:
+				if !ok {
+					return
+				}
+				worker := <-d.workers
+				logrus.Debugf(`分配任务[%v]到worker[%v]`, target, worker)
+				worker <- target
 			}
-			worker := <-d.workers
-			logrus.Debugf(`分配任务[%v]到worker[%v]`, target, worker)
-			worker <- target
 		}
-	}
+	}()
 }
--- pkg/dispatcher/dispatcher_test.go
+++ pkg/dispatcher/dispatcher_test.go
@@ -1,7 +1,22 @@
 package dispatcher
 
-import "testing"
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+)
 
 func TestNewDispatcher(t *testing.T) {
-
+	dispatcher := NewDispatcher(2)
+	dispatcher.Start(context.TODO())
+	go dispatcher.Dispatch(func() error {
+		fmt.Println("test1")
+		return nil
+	})
+	go dispatcher.Dispatch(func() error {
+		fmt.Println("test2")
+		return fmt.Errorf(`test %v`,`test`)
+	})
+	time.Sleep(time.Second*2)
 }
